a268beee57cfe53abd4afa70ca8d17641d41919a
lucas.ms(이문성)/kakao <lucas.ms@kakaocorp.com>
2025-03-26 18:50:53 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>타이핑 게임</h1>
+      </header>
+      <main>
+        <div className="game-container">
+          {/* 게임 컴포넌트들이 여기에 들어갈 예정입니다 */}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App; 
